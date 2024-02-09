68c96292c8b6e31d640576501813725f120a5a6a
Michał <mmroz@student.agh.edu.pl>
2024-02-09 19:15:12 +0100

    init v.2

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # picoForest
 Pichał wants, Pichał gets
+
+## How to run
+To compile run
+```shell
+  cmake .
+  make
+```
+and then run picoForest in build directory
+
+### Important stuff here I guess
+[GLFW](https://www.glfw.org/) is placed in lib/glfw-3.3.9 directory. It's compiled and linked by the main cmake file 
+
+### To do
+- [x] add glfw library
+- [ ] start project
+- [ ] pichał finishes my project
+- [ ] happy ending
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,46 @@
-#include <X11/Xlib.h>
+#include <GLFW/glfw3.h>
 #include <GL/gl.h>
-#include <GL/glx.h>
 #include <iostream>
 
-const int width = 800;
-const int height = 600;
-
-int xError(Display *display, XErrorEvent *event){
-    char errorMessage[1024];
-    XGetErrorText(display, event->error_code, errorMessage, sizeof(errorMessage));
-    std::cerr << "X Error: "<<errorMessage << std::endl;
-    return 0;
+void errCallback(int error, const char* description){
+    std::cerr << "GLFW Error: "<< description <<std::endl;
 }
 
-int main() {
-    Display *display = XOpenDisplay(nullptr);
-    if(!display){
-        std::cerr<<"Failed to open X display"<<std::endl;
-        return 1;
-    }
-    XSetErrorHandler((XErrorHandler)xError);
+int main(){
+    glfwSetErrorCallback(errCallback);
 
-    static int visual_atribs[] = {
-            GLX_RENDER_TYPE, GLX_RGBA_BIT, GLX_DRAWABLE_TYPE,
-            GLX_WINDOW_BIT, GLX_DOUBLEBUFFER, true, None
-    };
-
-    int fbcount;
-    GLXFBConfig* fbc = glXChooseFBConfig(display, DefaultScreen(display), visual_atribs, &fbcount);
-    if(!fbc){
-        std::cerr<<"Failed retrieving a framebuffer config" <<std::endl;
-        XCloseDisplay(display);
+    if(!glfwInit()){
+        std::cerr << "Failed to initialize GLFW" <<std::endl;
         return 1;
     }
 
-    XVisualInfo* vi = glXGetVisualFromFBConfig(display, fbc[0]);
-    Colormap cmap = XCreateColormap(display, RootWindow(display, vi->screen), vi->visual, AllocNone);
-
-    XSetWindowAttributes swa;
-    swa.colormap = cmap;
-    swa.event_mask = ExposureMask | KeyPressMask;
-    Window win = XCreateWindow(display, RootWindow(display, vi->screen), 0, 0, width, height,
-                               0, vi->depth, InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
-    XStoreName(display, win, "PicoForest");
-    GLXContext ctx = glXCreateContext(display, vi, nullptr, GL_TRUE);
-    glXMakeCurrent(display, win, ctx);
-    XMapWindow(display, win);
-
-    bool running = true;
-    XEvent event;
-    while(running){
-        while(XPending(display) > 0) {
-            XNextEvent(display, &event);
-            switch (event.type){
-                case KeyPress:
-                    running = false;
-                    break;
-            }
-        }
-        glXSwapBuffers(display, win);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Test", NULL, NULL);
+    if(!window){
+        std::cerr << "Failed to create GLFW window" <<std::endl;
+        glfwTerminate();
+        return 1;
     }
-    glXMakeCurrent(display, None, nullptr);
-    glXDestroyContext(display, ctx);
-    XDestroyWindow(display, win);
-    XCloseDisplay(display);
 
-    std::cout << "Hello, World!" << std::endl;
+    glfwMakeContextCurrent(window);
 
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, -0.5f);
+        glVertex2f(-0.5f, -0.5f);
 
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2d(0.5f, -0.5f);
+
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2d(0.0f, 0.5f);
+
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
