8c8b2f1da323d7278906a0aa76610f6543fa3ddb
Michał <mmroz@student.agh.edu.pl>
2024-02-16 16:51:55 +0100

    Kitty's classism

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ GLFWwindow *create_window() {
         exit(-1);
     }
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Test", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "pico Forest", NULL, NULL);
     if(!window){
         std::cerr << "Failed to create GLFW window" <<std::endl;
         glfwTerminate();
@@ -68,6 +68,8 @@ unsigned int indices[] = {
 
 };
 
+
+
 int load_shader(std::string filename, int kind) {
     std::ifstream ifs(filename);
     std::string shader_source(
@@ -95,9 +97,7 @@ int load_shader(std::string filename, int kind) {
     return shader;
 }
 
-int main(){
-    auto window = create_window();
-
+GLuint shader_program_setup(){
     auto vertex_shader = load_shader("src/shader.vert", GL_VERTEX_SHADER);
     auto fragment_shader = load_shader("src/shader.frag", GL_FRAGMENT_SHADER);
     auto shader_program = glCreateProgram();
@@ -117,37 +117,82 @@ int main(){
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
+    return shader_program;
+}
 
-    unsigned int vbo;
-    glGenBuffers(1, &vbo);
+class Kitty{
+public:
+    Kitty(unsigned int shaderProgram, int posX, int posY) : shaderProgram(shaderProgram){
+        this->load_vertices_kitty(posX, posY);
+        this->buffer_setup();
+    }
 
-    unsigned int ebo;
-    glGenBuffers(1, &ebo);
+    void render(){
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, sizeof(index)/sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+    }
 
-    unsigned int vao;
-    glGenVertexArrays(1, &vao);
+private:
+    float vert[sizeof(vertices)/sizeof(vertices[0])];
+    unsigned int index[sizeof(indices)/sizeof(indices[0])];
+    unsigned int VBO, EBO, VAO;
+    unsigned int shaderProgram;
+
+    void load_vertices_kitty(int x, int y){
+        int vertCount = sizeof(vertices)/sizeof(vertices[0]);
+        for(int i = 0; i < vertCount; i+=3){
+            this->vert[i] = vertices[i] + x;
+            this->vert[i+1] = vertices[i+1] + y;
+            this->vert[i+2] = vertices[i+2];
+        }
+        std::copy(std::begin(indices), std::end(indices), std::begin(index));
+    }
+
+    void buffer_setup(){
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(this->vert), this->vert, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+};
 
 
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
-    glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+int main(){
+    auto window = create_window();
+    
+    auto shader_program = shader_program_setup();
+    
+    Kitty cat(shader_program, 2, 2);
+    Kitty cat2(shader_program, 0,0);
 
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program);
-        glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+        cat.render();
+        cat2.render();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+
+
+    glDeleteProgram(shader_program);
     glfwTerminate();
+
     return 0;
 }
